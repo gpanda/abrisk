cbd456770d3f825c5795c0701a1feb01d62a3e33
Letian Ren <gpanda.next@gmail.com>
2018-02-02 01:42:40 +0800

    Optimise funcs.sh
    * refine the code
    * when network is disconnected, exit direct
    * when the current data file exists, exit direct

--- funcs.sh
+++ funcs.sh
@@ -1,36 +1,37 @@
 record_history () {
 
+    network_ok || exit
+
     time=`date +'%H%M'`
     weekday=`date +'%w'`
 
     if [ "$os" = "Linux" ]; then
         [ "$weekday" -eq 0 ] || [ "$weekday" -eq 6 ] \
-                && _date=$(date -d"last Fri" +'%Y%m%d') \
-                && $CMD > "$FOLDER/$_date" \
-                && exit
+                && _date=$(date -d"last Fri" +'%Y%m%d')
         [ "$time" -lt "900" ] \
-                && _date=$(date -d"1 day ago" +'%Y%m%d') \
-                && $CMD > "$FOLDER/$_date" \
-                && exit
+                && _date=$(date -d"1 day ago" +'%Y%m%d')
         [ "$time" -gt "1600" ] \
-                && _date=$(date +'%Y%m%d') \
-                && $CMD > "$FOLDER/$_date" \
-                && exit
+                && _date=$(date +'%Y%m%d')
     elif [ "$os" = "macOS" ]; then
         [ "$weekday" -eq 0 ] || [ "$weekday" -eq 6 ] \
-                && _date=$(date -v-fri +'%Y%m%d') \
-                && $CMD > "$FOLDER/$_date" \
-                && exit
+                && _date=$(date -v-fri +'%Y%m%d')
         [ "$time" -lt "900" ] \
-                && _date=$(date -v-1d +'%Y%m%d') \
-                && $CMD > "$FOLDER/$_date" \
-                && exit
+                && _date=$(date -v-1d +'%Y%m%d')
         [ "$time" -gt "1600" ] \
-                && _date=$(date +'%Y%m%d') \
-                && $CMD > "$FOLDER/$_date" \
-                && exit
+                && _date=$(date +'%Y%m%d')
     fi
+    f="$FOLDER/$_date"
+    file_exists "$f" || $CMD > "$f"
+    exit
+}
 
+network_ok () {
+    test_address=www.163.com
+    ping -c 1 $test_address > /dev/null 2>&1
+}
+
+file_exists () {
+    stat "$1" > /dev/null 2>&1
 }
 
 print_separator () {
