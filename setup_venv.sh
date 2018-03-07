7725b71ff215f4fd8ce8b6828bd10501aad00ebf
Letian Ren <gpanda.next@gmail.com>
2018-03-07 23:29:23 +0800

    Add dependencies for testing and 2to3

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip install virtualenv
-virtualenv .venv
+virtualenv --python="/usr/local/Cellar/python@2/2.7.14_1/bin/python2.7" .venv
 . .venv/bin/activate
 pip install requests
-pip install futures
+pip install futures coverage pylint caniusepython3 tox
