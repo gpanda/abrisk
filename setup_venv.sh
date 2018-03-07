b36311eeb4203ea002c7d8d1866f191d9f25dccc
Letian Ren <gpanda.next@gmail.com>
2018-03-07 23:42:42 +0800

    Update dependencies

--- setup_venv.sh
+++ setup_venv.sh
@@ -3,4 +3,4 @@ pip install virtualenv
 virtualenv --python="/usr/local/Cellar/python@2/2.7.14_1/bin/python2.7" .venv
 . .venv/bin/activate
 pip install requests
-pip install futures coverage pylint caniusepython3 tox
+pip install future coverage pylint caniusepython3 tox
