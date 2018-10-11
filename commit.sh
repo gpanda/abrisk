5d1565f7d4745e8462f2122595620bbcce51da39
Letian Ren <gpanda.next@gmail.com>
2018-10-11 22:40:15 +0800

    Add commit.sh to ease code commit

---
+++ commit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+git co dev
+git add .
+git staus
+git commit -a
+git co master
+git fetch
+git pull
+git co dev
+git rebase master
+git push
+git push myrepos
+git co master
+git merge dev
+git push
+git push myrepos
